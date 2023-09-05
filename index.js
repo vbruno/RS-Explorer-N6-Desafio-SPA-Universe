0ab2a23a39bb853f2743b0abe9f133fa2cfc593d
Bruno S Velho <bruno.velho@gmail.com>
2023-09-05 09:44:19 -0300

    Projeto Iniciado +Add lite-server +Add parte de rotas +Add navegação pagina +Estilização da pagina "home"

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Router } from "./src/js/router.js";
+
+console.log("Hello, world!");
+
+const router = new Router();
+
+router.add("/", "./src/pages/home.html");
+router.add("/universo", "./src/pages/universo.html");
+router.add("/exploracao", "./src/pages/exploracao.html");
+
+router.add(404, "./src/pages/404.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.handle();
---
+++ src/js/router.js
@@ -0,0 +1,26 @@
+export class Router {
+  routes = {};
+
+  add(routeName, page) {
+    this.routes[routeName] = page;
+  }
+
+  routes(event) {
+    event = event || window.event;
+    event.preventDefault();
+
+    window.history.pushState({}, "", event.target.href);
+
+    this.handle();
+  }
+
+  handle() {
+    const { pathname } = window.location;
+    const route = this.routes[pathname] || this.routes[404];
+    fetch(route)
+      .then((response) => response.text())
+      .then((html) => {
+        document.querySelector("#app").innerHTML = html;
+      });
+  }
+}
